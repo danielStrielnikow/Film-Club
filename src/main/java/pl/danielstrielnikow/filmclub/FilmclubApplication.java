c2d7d50e5fc3eda9801e323c1e261db95d639063
daniel <daniel.strielnikow@outlook.com>
2024-12-05 18:39:33 +0100

    First commit

---
+++ src/main/java/pl/danielstrielnikow/filmclub/FilmclubApplication.java
@@ -0,0 +1,13 @@
+package pl.danielstrielnikow.filmclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmclubApplication.class, args);
+    }
+
+}
