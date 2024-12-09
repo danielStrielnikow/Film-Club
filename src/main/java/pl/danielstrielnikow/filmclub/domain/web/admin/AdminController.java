b19bac88e9bf04a0368e34a683fe4d8cd529c6af
daniel <daniel.strielnikow@outlook.com>
2024-12-09 19:51:17 +0100

    Dodanie panelu administracyjnego

---
+++ src/main/java/pl/danielstrielnikow/filmclub/domain/web/admin/AdminController.java
@@ -0,0 +1,13 @@
+package pl.danielstrielnikow.filmclub.domain.web.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String getAdminPanel() {
+        return "admin/admin";
+    }
+}
