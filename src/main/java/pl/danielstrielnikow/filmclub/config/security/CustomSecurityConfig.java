a72df71e163f04bbbc085e1739fcdc8720038a90
daniel <daniel.strielnikow@outlook.com>
2024-12-15 16:31:33 +0100

    Konfiguracja wylogowania

--- src/main/java/pl/danielstrielnikow/filmclub/config/security/CustomSecurityConfig.java
+++ src/main/java/pl/danielstrielnikow/filmclub/config/security/CustomSecurityConfig.java
@@ -2,10 +2,12 @@ package pl.danielstrielnikow.filmclub.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -21,6 +23,9 @@ public class CustomSecurityConfig {
         ).formLogin(login -> login
                 .loginPage("/login")
                 .permitAll()
+        ) .logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
+                .logoutSuccessUrl("/login?logout").permitAll()
         );
         return http.build();
     }
