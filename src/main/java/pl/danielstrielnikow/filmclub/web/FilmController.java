36620795a659efa5520905944f0954b4ddef63a1
daniel <daniel.strielnikow@outlook.com>
2024-12-21 21:38:20 +0100

    Głosowanie - Widok i kontrolety

--- src/main/java/pl/danielstrielnikow/filmclub/web/FilmController.java
+++ src/main/java/pl/danielstrielnikow/filmclub/web/FilmController.java
@@ -1,6 +1,7 @@
 package pl.danielstrielnikow.filmclub.web;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,22 +9,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 import pl.danielstrielnikow.filmclub.domain.film.FilmService;
 import pl.danielstrielnikow.filmclub.domain.film.dto.FilmDto;
+import pl.danielstrielnikow.filmclub.domain.rating.RatingService;
 
 import java.util.Optional;
 
 @Controller
 public class FilmController {
     private final FilmService filmService;
+    private final RatingService ratingService;
 
-    public FilmController(FilmService filmService) {
+    public FilmController(FilmService filmService, RatingService ratingService) {
         this.filmService = filmService;
+        this.ratingService = ratingService;
     }
 
     @GetMapping("/film/{id}")
-    public String getFilm(@PathVariable long id, Model model) {
+    public String getMovie(@PathVariable long id,
+                           Model model,
+                           Authentication authentication) {
         FilmDto film = filmService.findFilmById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         model.addAttribute("film", film);
+        //Jeżeli użytkownik jest zalogowany
+        if (authentication != null) {
+            String currentUserEmail = authentication.getName();
+            //to wyszukujemy jego głos
+            Integer rating = ratingService.getUserRatingForFilm(currentUserEmail, id).orElse(0);
+            //i zapisujemy go w modelu
+            model.addAttribute("userRating", rating);
+        }
         return "film";
     }
 }
